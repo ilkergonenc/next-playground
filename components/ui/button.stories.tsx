98d2b2fb6c4b53e646c49a0128751fc3342dbb97
ilkergonenc <ilkergonenc@gmail.com>
2023-07-22 13:29:06 +0300

    test(storybook): button component

---
+++ components/ui/button.stories.tsx
@@ -0,0 +1,73 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Button } from './button'
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta: Meta<typeof Button> = {
+	title: 'Components/Button',
+	component: Button,
+	tags: ['autodocs'],
+	decorators: [
+		(Story) => (
+			<div className="h-full w-full bg-background p-4">
+				<Story />
+			</div>
+		),
+	],
+	parameters: {
+		layout: 'fullscreen',
+	},
+}
+
+export default meta
+type Story = StoryObj<typeof Button>
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+	render: (args) => <Button {...args} />,
+	args: {
+		children: 'Primary',
+		variant: 'default',
+		size: 'default',
+	},
+}
+
+export const Secondary: Story = {
+	args: {
+		children: 'Secondary',
+		variant: 'secondary',
+		size: 'default',
+	},
+}
+
+export const Detructive: Story = {
+	args: {
+		children: 'Detructive',
+		variant: 'destructive',
+		size: 'default',
+	},
+}
+
+export const Outline: Story = {
+	args: {
+		children: 'Outline',
+		variant: 'outline',
+		size: 'default',
+	},
+}
+
+export const Ghost: Story = {
+	args: {
+		children: 'Ghost',
+		variant: 'ghost',
+		size: 'default',
+	},
+}
+
+export const Link: Story = {
+	args: {
+		children: 'Link',
+		variant: 'link',
+		size: 'default',
+	},
+}
