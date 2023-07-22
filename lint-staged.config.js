4287329ab55df46e58531fb9148b9e50d5b6237c
ilkergonenc <ilkergonenc@gmail.com>
2023-07-22 10:48:02 +0300

    ci: implement husky & lint-staged

---
+++ lint-staged.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+const prettierCommand = 'prettier --write --cache'
+const typeCheckCommand = 'tsc --noEmit'
+
+// https://nextjs.org/docs/app/building-your-application/configuring/eslint#lint-staged
+const buildEslintCommand = (filenames) =>
+	`next lint --fix --file ${filenames
+		.map((f) => path.relative(process.cwd(), f))
+		.join(' --file ')}`
+
+module.exports = {
+	'*.{ts,tsx}': [prettierCommand, buildEslintCommand, typeCheckCommand],
+	'*.{js,jsx}': [prettierCommand, buildEslintCommand],
+	'*.json': [prettierCommand],
+}
